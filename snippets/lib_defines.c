3b9e51e51c86832d8557d89d364bae32838fee8f
Luis Geniole <luis.geniole@gmail.com>
2021-03-07 01:09:25 -0300

    Add linux-specific header to lib_defines.

--- snippets/lib_defines.c
+++ snippets/lib_defines.c
@@ -6,7 +6,7 @@
 /*   By: lpaulo-m <lpaulo-m@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/16 00:07:42 by lpaulo-m          #+#    #+#             */
-/*   Updated: 2021/03/04 17:56:40 by lpaulo-m         ###   ########.fr       */
+/*   Updated: 2021/03/07 01:08:55 by lpaulo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,19 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <stdbool.h>
 #include <fcntl.h>
 #include <sys/resource.h>
 #include <stdint.h>
+#include <limits.h>
+
+# if __linux__
+# include <linux/limits.h>
+# endif
 
 int main()
 {
-	printf("------ stdio.h ------\n", stdout);
+	printf("------ stdio.h ------\n");
 	printf("EOF: %d\n", EOF);
 	printf("_IOFBF: %d\n", _IOFBF);
 	printf("_IOLBF: %d\n", _IOLBF);
@@ -38,9 +42,9 @@ int main()
 	printf("stderr: %d\n", stderr);
 	printf("stdin: %d\n", stdin);
 	printf("stdout: %d\n", stdout);
-	printf("------ stdio.h ------\n", stdout);
+	printf("------ stdio.h ------\n");
 
-	printf("------ unistd.h ------\n", stdout);
+	printf("------ unistd.h ------\n");
 	printf("SEEK_CUR: %d\n", SEEK_CUR);
 	printf("SEEK_END: %d\n", SEEK_END);
 	printf("SEEK_SET: %d\n", SEEK_SET);
@@ -48,17 +52,17 @@ int main()
 	// printf("OPEN_MAX: %d\n", OPEN_MAX);
 	printf("_SC_IPV6 : %d\n", _SC_IPV6);
 	printf("_SC_REGEXP  : %d\n", _SC_REGEXP);
-	printf("------ unistd.h ------\n", stdout);
+	printf("------ unistd.h ------\n");
 
-	printf("------ stdlib.h ------\n", stdout);
+	printf("------ stdlib.h ------\n");
 	printf("NULL: %d\n", NULL);
 	printf("EXIT_FAILURE: %d\n", EXIT_FAILURE);
 	printf("EXIT_SUCCESS: %d\n", EXIT_SUCCESS);
 	printf("RAND_MAX: %d\n", RAND_MAX);
 	printf("MB_CUR_MAX: %d\n", MB_CUR_MAX);
-	printf("------ stdlib.h ------\n", stdout);
+	printf("------ stdlib.h ------\n");
 
-	printf("------ limits.h ------\n", stdout);
+	printf("------ limits.h ------\n");
 	printf("CHAR_BIT: %d\n", CHAR_BIT);
 	printf("SCHAR_MIN: %d\n", SCHAR_MIN);
 	printf("SCHAR_MAX: %d\n", SCHAR_MAX);
@@ -75,27 +79,27 @@ int main()
 	printf("LONG_MIN: %d\n", LONG_MIN);
 	printf("LONG_MAX: %d\n", LONG_MAX);
 	printf("ULONG_MAX: %d\n", ULONG_MAX);
-	// printf("SSIZE_MAX: %d\n", SSIZE_MAX);
-	printf("------ limits.h ------\n", stdout);
+	printf("SSIZE_MAX: %d\n", SSIZE_MAX);
+	printf("------ limits.h ------\n");
 
-	printf("------ stdbool.h ------\n", stdout);
+	printf("------ stdbool.h ------\n");
 	printf("true: %d\n", true);
 	printf("false: %d\n", false);
 	printf("__bool_true_false_are_defined: %d\n", __bool_true_false_are_defined);
-	printf("------ stdbool.h ------\n", stdout);
+	printf("------ stdbool.h ------\n");
 
-	printf("------ fcntl.h ------\n", stdout);
+	printf("------ fcntl.h ------\n");
 	printf("F_GETFD: %d\n", F_GETFD);
 	printf("F_GETFL: %d\n", F_GETFL);
 	printf("F_GETLK: %d\n", F_GETLK);
-	// printf("F_GETOWN: %d\n", F_GETOWN);
-	printf("------ fcntl.h ------\n", stdout);
+	printf("F_GETOWN: %d\n", F_GETOWN);
+	printf("------ fcntl.h ------\n");
 
-	printf("------ sys/resource.h ------\n", stdout);
+	printf("------ sys/resource.h ------\n");
 	printf("RLIMIT_NOFILE: %d\n", RLIMIT_NOFILE);
-	printf("------ sys/resource.h ------\n", stdout);
+	printf("------ sys/resource.h ------\n");
 
-	printf("------ stdint.h ------\n", stdout);
+	printf("------ stdint.h ------\n");
 	// printf("INTN_MIN: %d\n", INTN_MIN);
 	// printf("INTN_MAX: %d\n", INTN_MAX);
 	// printf("UINTN_MAX: %d\n", UINTN_MAX);
@@ -118,14 +122,14 @@ int main()
 	printf("SIG_ATOMIC_MIN: %d\n", SIG_ATOMIC_MIN);
 	printf("SIG_ATOMIC_MAX: %d\n", SIG_ATOMIC_MAX);
 
-	printf("SIZE_MAX: %d\n", SIZE_MAX);
+	printf("SIZE_MAX: %lu\n", SIZE_MAX);
 
 	printf("WCHAR_MIN: %d\n", WCHAR_MIN);
 	printf("WCHAR_MAX: %d\n", WCHAR_MAX);
 
 	printf("WINT_MIN: %d\n", WINT_MIN);
 	printf("WINT_MAX: %d\n", WINT_MAX);
-	printf("------ stdint.h ------\n", stdout);
+	printf("------ stdint.h ------\n");
 
 	return (0);
 }
